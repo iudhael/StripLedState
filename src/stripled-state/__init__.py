83ef6a6680a83831e16f5ec3fed0761e11ef6769
iudhael <adikpetoiudhael@gmail.com>
2025-11-02 14:51:45 +0100

    init

---
+++ src/stripled-state/__init__.py
@@ -0,0 +1,22 @@
+"""StripLed State - Python library for MecaMate stripled signalisation.
+"""
+
+from ledsignalisation import addr_stripled_signalisation_non_bloquant_V2
+# Version info
+__version__ = "0.1.0"
+__author__ = "MecaMate StripLed State"
+__description__ = "MecaMate stripled signalisation"
+
+# Public API exports
+__all__ = [
+    "turn_off_all_stripled",
+    "pre_operational",
+    "ready",
+    "emergency_stop",
+    "ready_to_go",
+    "turning",
+    "braking",
+    "reverse",
+    "hello"
+
+]
