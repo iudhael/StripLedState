ed735dff69a1287e584a99d8d3a8df694cdd7f0f
iudhael <adikpetoiudhael@gmail.com>
2025-11-15 20:07:55 +0100

    test

--- src/StripLedState/addr_stripled_signalisation_non_bloquant_V2.py
+++ src/StripLedState/addr_stripled_signalisation_non_bloquant_V2.py
@@ -163,7 +163,7 @@ class AddrStripLedSignalisationNonBloquantV2:
     def pre_operational(self, global_brightness):
         """print("pre_operational ...")"""
         current_time = time.time()
-        duration = 1
+        duration = 3
         if self.pre_operational_fading_brightness  <= global_brightness and self.pre_operational_stripled_is_on == False:
             if current_time - self.pre_operational_fading_last_time / duration > 0.01:
                 self.all_stripled.brightness = (global_brightness / duration) * (current_time - self.pre_operational_fading_last_time)
